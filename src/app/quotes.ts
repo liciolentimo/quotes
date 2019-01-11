2e1ea7ec676995c87d6adfd0fd08715d99c97c16
liciolentimo <lentimo@gmail.com>
2019-01-11 17:16:06 +0300

    Resolved conflict with github pages

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     public showDetails:boolean;
-    constructor(public name:string,public author:string,public quote:string,public dateCreated:Date){
+    constructor(public personName:string,public authorName:string,public quoteName:string,public dateCreated:Date){
         this.showDetails=false;
     }
 }
