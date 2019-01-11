f22fcc4bfcfe5b8d3a89535cabdc796b29b2401c
liciolentimo <lentimo@gmail.com>
2019-01-11 11:04:30 +0300

    Created the constructor with objects and datatypes of each

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,6 @@
 export class Quotes {
+    public showDetails:boolean;
+    constructor(public name:string,public author:string,public quote:string,public dateCreated:Date){
+        this.showDetails=false;
+    }
 }
