1c92164ea776ff4e8c80b266ece164b672eb8082
liciolentimo <lentimo@gmail.com>
2019-01-12 15:18:40 +0300

    Added dateCreated and votes objects to the constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     public showDetails:boolean;
-    constructor(public personName:string,public authorName:string,public quoteName:string,public dateCreated:Date){
+    constructor(public personName:string,public authorName:string,public quoteName:string,public  dateCreated:Date,public votes:number){
         this.showDetails=false;
     }
 }
