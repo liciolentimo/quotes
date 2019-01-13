0e63c7ffb6489cf2b89bd60a328d002c1e148fa8
liciolentimo <lentimo@gmail.com>
2019-01-13 14:02:31 +0300

    Added directive highest-votes

---
+++ src/app/highest-votes.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighestVotesDirective } from './highest-votes.directive';
+
+describe('HighestVotesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighestVotesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
