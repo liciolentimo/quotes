666177a127ece39bc572ca83c307e5b688e34d63
liciolentimo <lentimo@gmail.com>
2019-01-13 14:09:17 +0300

    Customized fontstyle and color

--- src/app/highest-votes.directive.ts
+++ src/app/highest-votes.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighestVotes]'
 })
 export class HighestVotesDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.fontStyle='bold';
+  		this.elem.nativeElement.style.color='#800000';
+  	
+  }
 
 }
